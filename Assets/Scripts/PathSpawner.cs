c548bebcfc194358d934bdaf6c83cf87b050b61a
Dominik Augustin <augustindominik@hotmail.sk>
2024-01-13 22:16:20 +0100

    moved path spawner

--- Assets/Scripts/PathSpawner.cs
+++ Assets/Scripts/PathSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathSpawner : MonoBehaviour
+public class PathSpawner : SpawnerBase
 {
 
     public float objectsSpeed = 10.0f;
@@ -17,6 +17,9 @@ public class PathSpawner : MonoBehaviour
     public float leftSurroundingsXCenter = 20.0f;
     public float rightSurroundingsXCenter = -20.0f;
 
+
+    private float roadAccumulator = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,13 +29,19 @@ public class PathSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (spawnAccumulator >= objectsSpeed)
+        if(spawnEnabled == false)
         {
-            spawnTrio();
-            spawnAccumulator = 0.0f;
-        } else {
-            spawnAccumulator += 1;
+            return;
         }
+
+        objectsSpeed = GameManagerScript.Instance.gameSpeed;
+        //if (spawnAccumulator >= 6)
+        //{
+            spawnTrio();
+        //    spawnAccumulator = 0.0f;
+        //} else {
+       //     spawnAccumulator += 0.01f;
+        //}
     }
 
     public void spawnTrio()
@@ -44,14 +53,22 @@ public class PathSpawner : MonoBehaviour
     public void spawnRoad()
     {
         var roadPrefab = roadPrefabs[Random.Range(0, roadPrefabs.Count)];
+
+        if (Physics.Raycast(transform.position, Vector3.back, 6f, spawnLayer))
+        {
+            return;
+        }
         
         GameObject newObject = Instantiate(roadPrefab);
 
-        newObject.transform.position = new Vector3( transform.position.y, transform.position.y, transform.position.z);
+        newObject.transform.position = new Vector3( transform.position.x+5, transform.position.y, transform.position.z);
 
         var xScale = 10 / newObject.GetComponent<BoxCollider>().size.x;
-        newObject.transform.localScale = new Vector3(xScale, 1, 1);
-        newObject.AddComponent<EnvironmentObjectScript>();
+        newObject.transform.localScale = new Vector3(xScale, 1, 2.781918f);
+        //newObject.AddComponent<EnvironmentObjectScript>();
+        newObject.layer = LayerMask.NameToLayer("Ground");
+
+        
     }
 
     public void spawnSurroundings()
@@ -67,7 +84,7 @@ public class PathSpawner : MonoBehaviour
         GameObject newObject = Instantiate(gameObject);
 
         var spawnPositionX = Random.value * 10.0f;
-        var positionX = XlocationCenter > 0 ? spawnPositionX + XlocationCenter : spawnPositionX - XlocationCenter;
+        var positionX = XlocationCenter > 0 ? spawnPositionX + XlocationCenter :  XlocationCenter - spawnPositionX ;
         
         newObject.transform.position = new Vector3(positionX, transform.position.y-0.01f, transform.position.z);
         newObject.transform.localScale = new Vector3(1, 1, 1);
